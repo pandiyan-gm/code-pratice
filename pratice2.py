b76dfd48f5284a1a53a6d57ee5d7291af03cac24
Pandiyan G M <pandiya910@gmail.com>
2025-01-16 09:26:20 +0530

    Add files via upload

---
+++ pratice2.py
@@ -0,0 +1,11 @@
+#Question 2: This program checks whether the given num is postive or negative
+
+num=int(input("enter a number:  ")) #This line get the input from the user
+
+#The below condition checks whether the given number is pos or neg
+if num>0:
+    print("Postive")
+elif num==0:
+    print("Zero")
+else:
+    print("Negative")

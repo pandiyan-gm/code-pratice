e28b8c7d2566d2a3744a9869007e0d5ed26e81d2
Pandiyan G M <pandiya910@gmail.com>
2025-01-17 19:49:25 +0530

    Add files via upload

---
+++ pratice11.py
@@ -0,0 +1,12 @@
+#Question no 11: Array Rotation based on the given Nth value
+
+def rotate_arr(arr,n):
+    n=n%len(arr)
+    rotated_array=arr[n:]+arr[:n]
+    return rotated_array
+
+arr=list(map(int,input("enter the numbers: ").split( )))
+n=int(input("enter the number of rotations: "))
+result=rotate_arr(arr,n)
+
+print(f"After {n} rotations {result}")

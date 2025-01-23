7f278ce6b9b694a5642f56c52b87f2a72235b5c0
Pandiyan G M <pandiya910@gmail.com>
2025-01-23 20:52:39 +0530

    Update pratice9.py

--- pratice9.py
+++ pratice9.py
@@ -1,9 +1,9 @@
 #Question 9. This question prints the unique and duplicate values separately present in the list
 
-arr1=[2,4,6,2,6,3,5,4,7,8,9]  #this is the list contains values
-unique_values=set()    #creating empty sets
+arr1=[2,4,6,2,6,3,5,4,7,8,9]  
+unique_values=set()   
 duplicate_values=set()
-for i in arr1:  #looping through the list
+for i in arr1:  
     if i in unique_values:  
         duplicate_values.add(i)  #adding values in the set
     else:

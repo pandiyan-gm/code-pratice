56725257752834b925773477230b084e936cc243
Pandiyan G M <pandiya910@gmail.com>
2025-01-23 21:27:15 +0530

    Update pratice8.py

--- pratice8.py
+++ pratice8.py
@@ -1,1 +1,16 @@
+#question 8: Age group classifcation
 
+def age_grp(age):
+    if age<0:       #using Conditions to validate the age
+        print("Invalid age")
+    elif age<=12:
+        print("Child")
+    elif age<=19:
+        print("Teenager")
+    elif age<=64:
+        print("Adult")
+    else:
+        print("senior")
+age=int(input("Enter the age: "))
+age_grp(age)
+    

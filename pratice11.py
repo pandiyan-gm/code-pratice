e1d3daaa15e3ebdd5829f5828d334e5af82df512
Pandiyan G M <pandiya910@gmail.com>
2025-01-23 20:44:16 +0530

    Update pratice11.py

--- pratice11.py
+++ pratice11.py
@@ -2,11 +2,11 @@
 
 def rotate_arr(arr,n):
     n=n%len(arr)
-    rotated_array=arr[n:]+arr[:n]
+    rotated_array=arr[n:]+arr[:n]       #performing rotation logic 
     return rotated_array
 
 arr=list(map(int,input("enter the numbers: ").split( )))
 n=int(input("enter the number of rotations: "))
 result=rotate_arr(arr,n)
 
-print(f"After {n} rotations {result}")
+print(result)

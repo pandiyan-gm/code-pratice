b723ccbb6ff7ce97dababb05ecfdb5656aca1686
Pandiyan G M <pandiya910@gmail.com>
2025-01-23 20:46:37 +0530

    Update pratice12.py

--- pratice12.py
+++ pratice12.py
@@ -1,12 +1,13 @@
+#This program checks the given words are anagrams are not 
 def are_anagrams(str1,str2):
-    str1=str1.replace(" ","").lower()
+    str1=str1.replace(" ","").lower() #strings are converted into lowercase
     str2=str2.replace(" ","").lower()
-    return sorted(str1)==sorted(str2)
+    return sorted(str1)==sorted(str2)   #sorting the strings 
 
-word1=input("enter a word: ")
+word1=input("enter a word: ")      
 word2=input("enter a word: ")
 
-if are_anagrams(word1,word2):
+if are_anagrams(word1,word2):     
     print("the given words are anagrams")
 else:
     print("the given words are  not anagrams")
